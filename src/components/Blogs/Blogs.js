bc28285f54edac37d5a1b2a05951305d5ecb94cc
RaisaMarzana <chowdhuryraisa530@gmail.com>
2022-04-05 20:29:52 +0600

    blogs changed

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,19 +4,19 @@ const Blogs = () => {
     return (
         <div>
             <div class="p-6">
-                <div class="bg-white p-6 rounded-lg shadow-lg">
+                <div class="bg-blue-100 p-6 rounded-lg shadow-lg">
                     <h2 class="text-xl font-bold mb-2 text-gray-800">1. What is the purpose of Context API ?</h2>
                     <p class="text-gray-700">The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
                 </div>
             </div>
             <div class="p-6">
-                <div class="bg-white p-6 rounded-lg shadow-lg">
+                <div class="bg-blue-100 p-6 rounded-lg shadow-lg">
                     <h2 class="text-xl font-bold mb-2 text-gray-800">2. What is Semantic Tag ?</h2>
                     <p class="text-gray-700"> A semantic element clearly describes its meaning to both the browser and the developer. Examples of non-semantic elements: div and span - Tells nothing about its content. Examples of semantic elements: form, table, and article - Clearly defines its content.</p>
                 </div>
             </div>
             <div class="p-6">
-                <div class="bg-white p-6 rounded-lg shadow-lg">
+                <div class="bg-blue-100 p-6 rounded-lg shadow-lg">
                     <h2 class="text-xl font-bold mb-2 text-gray-800">3. What are the differences between inline, block and inline block elements ?</h2>
                     <p class="text-gray-700 font-bold">Inline</p>
                     <p class="text-gray-700">Inline displays the element inline or on the same line. In other words, inline elements do NOT start on a new line and only takes up as much width as its content. So, if you try to set any width and height, it will have no effects.</p>
