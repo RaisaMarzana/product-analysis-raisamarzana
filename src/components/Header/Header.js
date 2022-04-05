0f0dc867493979ab77432e551dde97ac948fee2a
RaisaMarzana <chowdhuryraisa530@gmail.com>
2022-04-05 20:20:22 +0600

    all button changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
         <div className='p-7 flex flex-col justify-center items-center'>
             <div>
                 <ul>
-                    <Link className='mr-2 rounded-lg px-5 py-2 bg-pink-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/">HOME</Link>
-                    <Link className='mr-2 rounded-lg px-5 py-2 bg-pink-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/reviews">REVIEWS</Link>
-                    <Link className='mr-2 rounded-lg px-5 py-2 bg-pink-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/dashboard">DASHBOARD</Link>
-                    <Link className='mr-2 rounded-lg px-5 py-2 bg-pink-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/blogs">BLOGS</Link>
+                    <Link className='mr-2 rounded-lg px-5 py-2 bg-blue-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/">HOME</Link>
+                    <Link className='mr-2 rounded-lg px-5 py-2 bg-blue-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/reviews">REVIEWS</Link>
+                    <Link className='mr-2 rounded-lg px-5 py-2 bg-blue-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/dashboard">DASHBOARD</Link>
+                    <Link className='mr-2 rounded-lg px-5 py-2 bg-blue-200 text-black hover:text-black hover:shadow-lg hover:font-bold' to="/blogs">BLOGS</Link>
                 </ul>
             </div>
         </div >
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
         <div>
             <div className='flex items-center justify-center'>
                 <div>
-                    <h1 className='text-6xl font-bold '>New <span className='text-pink-600'>Headphones</span></h1>
+                    <h1 className='text-6xl font-bold '>New <span className='text-yellow-600'>Headphones</span></h1>
                     <h1 className='text-6xl font-bold text-blue-500'><span>Future of Headphones</span></h1>
                     <p className='my-5 w-96'>Headphones are a pair of small loudspeaker drivers worn on or around the head over a user's ears. They are electroacoustic transducers, which convert an electrical signal to a corresponding sound.</p>
                 </div>
@@ -31,7 +31,7 @@ const Home = () => {
                 }
             </div>
             <div className='flex justify-center my-5'>
-                <button onClick={() => navigate('/reviews')} className=' rounded-lg px-5 py-2 bg-pink-500 text-white hover:text-black hover:shadow-lg hover:font-bold'>See All</button>
+                <button onClick={() => navigate('/reviews')} className=' rounded-lg px-5 py-2 bg-blue-500 text-white hover:text-black hover:shadow-lg hover:font-bold'>See All</button>
             </div>
         </div>
     );
