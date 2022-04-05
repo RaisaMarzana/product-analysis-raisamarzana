8dd1ff033ffcc4403309c9ede70cec77e0f22d9a
RaisaMarzana <chowdhuryraisa530@gmail.com>
2022-04-05 20:26:46 +0600

    dashboard changed

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -19,7 +19,7 @@ const DashBoard = () => {
                 </div>
                 <div>
                     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                        <Line type="monotone" dataKey="investment" stroke="#ec4899" />
+                        <Line type="monotone" dataKey="investment" stroke="#3b82f680" />
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                         <XAxis dataKey="month" />
                         <YAxis />
@@ -33,7 +33,7 @@ const DashBoard = () => {
                 </div>
                 <div>
                     <BarChart width={600} height={300} data={data}>
-                        <Bar dataKey="sell" fill="#ec4899" />
+                        <Bar dataKey="sell" fill="#3b82f680" />
                         <XAxis dataKey="month" />
                         <YAxis />
                         <Tooltip />
